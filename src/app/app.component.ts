0bc9b07d8a8051bd8d00ac27ef3f1f0bbb67ca45
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-01-15 17:19:03 -0300

    👌 refactor: Remoção código não utilizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'dio-course-manager';
-
-  name: string = 'Nome usuário';
-}
+export class AppComponent {}
