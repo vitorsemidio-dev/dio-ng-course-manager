79b9512d8104e6e83dc778ae05ad5c15681accf1
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-01-15 17:04:44 -0300

    ✨ feat: Criação Componente CourseList

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseListComponent } from './course-list/course-list.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CourseListComponent],
   imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
