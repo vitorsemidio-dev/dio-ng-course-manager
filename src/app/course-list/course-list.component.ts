0049fe0afe477014507e8f1e30bcfb1032bb66bb
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-01-15 17:19:33 -0300

    📚 Edu: Ciclos de vida do componente (ngOnInit)

--- src/app/course-list/course-list.component.ts
+++ src/app/course-list/course-list.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from './course';
 
 @Component({
   selector: 'app-course-list',
   templateUrl: './course-list.component.html',
-  styleUrls: ['./course-list.component.scss']
+  styleUrls: ['./course-list.component.scss'],
 })
 export class CourseListComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  courses: Course[] = [];
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.courses = [
+      {
+        id: 1,
+        name: 'Angular Forms',
+        imageUrl: '',
+        price: 100.0,
+        code: 'Ang-Form',
+        duration: 120,
+        rating: 4.5,
+        releaseDate: 'December, 22, 2020',
+      },
+      {
+        id: 2,
+        name: 'Angular Http',
+        imageUrl: '',
+        price: 50.4,
+        code: 'Ang-Http',
+        duration: 80,
+        rating: 3.8,
+        releaseDate: 'November, 22, 2020',
+      },
+    ];
   }
-
 }
