86db99ffff7f5301a4dbf1d98898576abc257415
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-01-15 22:12:15 -0300

    👌 refactor: Remoção seletor de CourseList

--- src/app/course-list/course-list.component.ts
+++ src/app/course-list/course-list.component.ts
@@ -4,7 +4,6 @@ import { Course } from './course';
 import { CourseService } from './course.service';
 
 @Component({
-  selector: 'app-course-list',
   templateUrl: './course-list.component.html',
   styleUrls: ['./course-list.component.scss'],
 })
