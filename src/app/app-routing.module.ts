929f95777b406952158cb709ef7c0e73e81226bd
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-01-15 22:23:02 -0300

    ✨ feat: Criação CourseInfo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { CourseInfoComponent } from './course-info/course-info.component';
 import { CourseListComponent } from './course-list/course-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
@@ -13,6 +14,10 @@ const routes: Routes = [
   {
     path: 'courses',
     component: CourseListComponent,
+  },
+  {
+    path: 'courses/info/:id',
+    component: CourseInfoComponent,
   },
   {
     path: '**',
