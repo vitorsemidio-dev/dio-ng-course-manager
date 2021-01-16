23ade9316d456a3d77bc55072b12d126ab424653
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-01-15 22:11:07 -0300

    ✨ refector: Renderização componente por Rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { CourseListComponent } from './course-list/course-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'courses',
+    pathMatch: 'full',
+  },
+  {
+    path: 'courses',
+    component: CourseListComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
